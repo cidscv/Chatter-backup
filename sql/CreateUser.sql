668a89b76a3da2df2c7aaefe50066374789d7871
XavierChanth <xchanthavong@gmail.com>
2020-03-26 23:48:47 -0400

    upload stored procedure statements

---
+++ sql/CreateUser.sql
@@ -0,0 +1,40 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE CreateUser
+	-- Add the parameters for the stored procedure here
+	@username NVARCHAR(500),
+	@password NVARCHAR(500),
+	@userid int OUTPUT
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	INSERT INTO [user]
+	(username, password)
+	VALUES
+	(@username, @password);
+
+	SET @userid = SCOPE_IDENTITY();
+END
+GO
